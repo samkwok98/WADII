d8feb09d8301e052cd90451d4fda071646b0e196
aidan445 <aidancheah.2021@scis.smu.edu.sg>
2022-11-12 14:27:28 +0800

    amended about.php
    Added bootstrap import call to CDN

--- about.php
+++ about.php
@@ -1,4 +1,6 @@
-<?php include 'header.php';?>
+<?php include 'header.php'; ?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
